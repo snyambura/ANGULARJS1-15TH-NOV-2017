c58af236672e152350fdfadfeb63c6ab05160cea
snyambura <snyambura94@gmail.com>
2017-11-16 09:19:13 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var angularApp = angular.module("angularApp", []);
+
+angularApp.controller("mainCtrl", function ($scope) {
+    $scope.names = "Salome";
+
+    $scope.colors = [
+        {
+            "Name" : "Blue",
+            "hexcode" : "0000ff"
+        },
+        {
+            "Name" : "Green",
+            "hexcode" : "008000"
+        },
+        {
+            "Name" : "red",
+            "hexcode" : "ff0000"
+        },
+        {
+            "Name" : "black",
+            "hexcode" : "000000"
+        }
+    ];
+});
